3a40d5bf3f0039cb9d07301e326aecfaba98de7f
suppin <rooreltuku@gmail.com>
2017-07-24 14:17:42 +0900

    deviseを導入 root#indexでトップページを表示

--- Gemfile
+++ Gemfile
@@ -52,3 +52,5 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+#kashiwagiadd
+gem 'devise', git: 'https://github.com/gogovan/devise.git', branch: 'rails-5.1'
--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,10 @@ module FlowerHappy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    # タイムゾーンを東京(JST +9:00)に設定
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
